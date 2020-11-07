32d3a8637a09a37c14f5adee1ef118c8e95c3223
Deivaras <44872100+Deivaras@users.noreply.github.com>
2020-11-07 21:04:18 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const TITLE = 'React Graphql Github Client';
+  
+  const GET_ORGANIZATION = `
+    {
+      organization(login: "the-road-to-learn-react") {
+        name
+        url
+      }
+    }
+  `;
+  
+  const [ path, setPath ] = useState({ 
+    path: 'the-road-to-learn-react',
+    url: 'the-road-to-learn-react'
+  });
+
+  const axiosGitHubGraphQL = axios.create({
+    baseURL: 'https://api.github.com/graphql',
+    headers: {
+      Authorization: `bearer ${
+        process.env.REACT_APP_GITHUB_PERSONALACCESS_TOKEN
+      }`
+    }
+  })
+
+  const onFetchFromGitHub = () => {
+    axiosGitHubGraphQL
+      .post('', { query: GET_ORGANIZATION })
+      .then(result => console.log(result));
+  };
+  
+  useEffect(() => {
+    // fetch data
+    onFetchFromGitHub();
+  }, [onFetchFromGitHub]);
+
+  const onChange = (e) =>
+    setPath({
+      ...path,
+      [e.target.name]: e.target.value
+    });
+
+  const onSubmit = e => {
+    e.prevetnDefault();
+  }
+
+  console.log(path);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{TITLE}</h1>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="url">
+          Show open issues for https://github.com/
+        </label>
+        <input 
+          id="url"
+          type="text"
+          name="url"
+          value={path.url}
+          onChange={onChange}
+          style={{width: 300}}
+        />
+        <button type="submit">Search</button>
+      </form>
+      <hr/>
     </div>
   );
 }
